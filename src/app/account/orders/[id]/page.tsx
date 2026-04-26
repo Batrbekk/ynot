427d28cb838f5dcce0cd5cd17cc37c0efe3d1936
batrbekk <batrbekk@gmail.com>
2026-04-27 01:45:02 +0500

    feat(account): /account/orders/[id] detail page

---
+++ src/app/account/orders/[id]/page.tsx
@@ -0,0 +1,14 @@
+import { notFound } from "next/navigation";
+import { getOrderById } from "@/lib/data/orders";
+import { OrderDetailLayout } from "@/components/account/order-detail-layout";
+
+interface PageProps {
+  params: Promise<{ id: string }>;
+}
+
+export default async function OrderDetailPage({ params }: PageProps) {
+  const { id } = await params;
+  const order = await getOrderById(id);
+  if (!order) notFound();
+  return <OrderDetailLayout order={order} />;
+}
