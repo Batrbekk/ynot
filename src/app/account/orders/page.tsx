fd447ff3f24d2faa9e68d20bda54e587456c1589
batrbekk <batrbekk@gmail.com>
2026-04-27 01:45:02 +0500

    feat(account): /account/orders list page

---
+++ src/app/account/orders/page.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+import { getOrdersForCurrentUser } from "@/lib/data/orders";
+import { OrderListItem } from "@/components/account/order-list-item";
+
+export default async function AccountOrdersPage() {
+  const orders = await getOrdersForCurrentUser();
+  if (orders.length === 0) {
+    return (
+      <p className="text-[14px] text-foreground-secondary py-12">
+        You haven&rsquo;t placed any orders yet.
+      </p>
+    );
+  }
+  return (
+    <ul className="border-t border-border-light">
+      {orders.map((o) => (
+        <OrderListItem key={o.id} order={o} />
+      ))}
+    </ul>
+  );
+}
